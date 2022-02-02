35c39caeb8079399a0f7a223ba37ed49d71d9a4a
sebcp <scaphismm@gmail.com>
2022-02-02 20:55:09 -0300

    Made a function for the template

--- bot.py
+++ bot.py
@@ -2,17 +2,19 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.open("img-base.png")
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("jarmstrongbold.ttf", 16)
-msg = "covid-19"
-w,h = font.getsize(msg)
-x1 = 465
-y1 = 180
-x2 = 570
-y2 = 215
-x = (x1 + x2)/2-w
-y = (y1 + y2)/2-h
-draw.rectangle([x1, y1, x2, y2],outline ="red")
-draw.text(((x),(y)), msg, font=font, fill="black")
-img.save('sample-out.png')
+MAX_CHAR_16 = 6
+
+def template(msg):
+    img = Image.open("img-base.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("jarmstrongbold.ttf", 16)
+    w,h = font.getsize(msg)
+    x1 = 464
+    y1 = 180
+    x2 = 572
+    y2 = 215
+    x = (x1 + x2 - w)/2
+    y = (y1 + y2 - h)/2
+    #draw.rectangle([x1, y1, x2, y2],outline ="red")
+    draw.text(((x),(y)), msg, font=font, fill="black")
+    img.save('sample-out.png')
